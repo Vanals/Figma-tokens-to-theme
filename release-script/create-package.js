4e27e8034edce9cec5dbcc2690e1f02a8952f471
Vanals <marco91vanali@gmail.com>
2022-02-15 13:57:44 +0000

    debugging

--- release-script/create-package.js
+++ release-script/create-package.js
@@ -3,57 +3,56 @@ const fs = require('fs')
 
 const createPackage = function () {
   // TODO update with packageName
-  console.log('running create Package')
-  // console.log('\n 👩‍💻Creating packageName...👨‍💻 \n')
+  console.log('\n 👩‍💻Creating packageName...👨‍💻 \n')
   // Create package folder
   // TODO replace with package name folder
-  fs.mkdir('./package-release-folder', { recursive: true }, (err) => {
-    if (err) throw err;
-    // TODO replace with packageName
-    console.log('Created packageName folder ✅');
-  });
+  // fs.mkdir('./package-release-folder', { recursive: true }, (err) => {
+  //   if (err) throw err;
+  //   // TODO replace with packageName
+  //   console.log('Created packageName folder ✅');
+  // });
   
   // TODO replace name with package name
   // TODO LICENSE TYPE?
   // TODO handle package versioning, pick up the version in the package.json and after updating it use it for the new package.json 
   // Create a pr for updating repo package.json with new version?
   // TODO specify author and.. anything else?
-  const packageJson = `
-  {
-    "name": "package-name",
-    "version": "1.0.0",
-    "license": "MIT",
-    "author": ""
-  }
-  `
+  // const packageJson = `
+  // {
+  //   "name": "package-name",
+  //   "version": "1.0.0",
+  //   "license": "MIT",
+  //   "author": ""
+  // }
+  // `
 
   // Add package.json
-  fs.writeFile('./package-release-folder/package.json', packageJson, (err) => {
-    if (err) throw err;
-    console.log('Created package.json ✅');
-  })
+  // fs.writeFile('./package-release-folder/package.json', packageJson, (err) => {
+  //   if (err) throw err;
+  //   console.log('Created package.json ✅');
+  // })
   
   // Add NewsKit theme json file
-  fs.copyFile('./build/nk-theme/figma-tokens.json', './package-release-folder/nk-themes.json', (err) => {
-    if (err) throw err;
-    console.log('Added NewsKit themes json ✅');
-  });
+  // fs.copyFile('./build/nk-theme/figma-tokens.json', './package-release-folder/nk-themes.json', (err) => {
+  //   if (err) throw err;
+  //   console.log('Added NewsKit themes json ✅');
+  // });
   
   // TODO TO ES6?
   // Add index.js to export files
-  const importExportList = `const NkThemes = require('./nk-themes.json');
+  // const importExportList = `const NkThemes = require('./nk-themes.json');
 
-  const themes = {
-    NkThemes
-  }
+  // const themes = {
+  //   NkThemes
+  // }
 
-  module.exports = themes
-  `
+  // module.exports = themes
+  // `
 
-  fs.writeFile('./package-release-folder/index.js', importExportList, (err) => {
-    if (err) throw err;
-    console.log('Added index.js with exports ✅');
-  })
+  // fs.writeFile('./package-release-folder/index.js', importExportList, (err) => {
+  //   if (err) throw err;
+  //   console.log('Added index.js with exports ✅');
+  // })
 }
 
 createPackage()
